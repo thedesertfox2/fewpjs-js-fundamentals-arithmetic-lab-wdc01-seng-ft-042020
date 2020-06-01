b0f24522e17640bbe36670ea4aa789778f293132
thedesertfox2 <alexander.gautherin@uconn.edu>
2020-05-31 20:43:36 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+const random = Math.random();
+
+const mod = 14 % 5;
+
+const max = Math.max(1, 20, 4)
+
+
